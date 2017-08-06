50153a4eb583920b5a566fcd2bddfd781dcd5700
phatpham9 <phatpham9@gmail.com>
2017-08-06 14:43:36 +0700

    feat(Close #9): implement sources api

--- src/sources.js
+++ src/sources.js
@@ -1,1 +1,55 @@
-export default {};
+// eslint-disable-next-line import/no-extraneous-dependencies
+import qs from 'qs';
+
+import { $get, $put, $delete } from './base';
+
+const PATH = 'sources';
+
+const getSources = async (queryParams) => {
+  const search = queryParams && typeof queryParams === 'object' ? qs.stringify(queryParams, { addQueryPrefix: true }) : '';
+
+  try {
+    const res = await $get(`${PATH}${search}`);
+
+    return res;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getSource = async (id) => {
+  try {
+    const res = await $get(`${PATH}/${id}`);
+
+    return res;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const subscribe = async (id) => {
+  try {
+    const res = await $put(`${PATH}/${id}/subscription`);
+
+    return res;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const unsubscribe = async (id) => {
+  try {
+    const res = await $delete(`${PATH}/${id}/subscription`);
+
+    return res;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export {
+  getSources,
+  getSource,
+  subscribe,
+  unsubscribe,
+};
